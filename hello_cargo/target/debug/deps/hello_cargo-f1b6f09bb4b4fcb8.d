612409d44c9f907049bc58f00e8df9f8c2ae3f42
vkhristenko <vdkhristenko1991@gmail.com>
2018-04-16 22:36:07 +0200

    doing rm

--- hello_cargo/target/debug/deps/hello_cargo-f1b6f09bb4b4fcb8.d
+++
@@ -1,5 +0,0 @@
-/Users/vk/software/rust/hello_cargo/target/debug/deps/hello_cargo-f1b6f09bb4b4fcb8: src/main.rs
-
-/Users/vk/software/rust/hello_cargo/target/debug/deps/hello_cargo-f1b6f09bb4b4fcb8.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/vk/software/rust/hello_cargo/target/debug/hello_cargo.dSYM: /Users/vk/software/rust/hello_cargo/src/main.rs
