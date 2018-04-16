36a2934e35ad913dc23c1e7108931dd9ad1e3414
vkhristenko <vdkhristenko1991@gmail.com>
2018-04-16 22:37:37 +0200

    rm

--- guessing_game/target/debug/deps/guessing_game-36b0db37a4334753.d
+++
@@ -1,5 +0,0 @@
-/Users/vk/software/rust/guessing_game/target/debug/deps/guessing_game-36b0db37a4334753: src/main.rs
-
-/Users/vk/software/rust/guessing_game/target/debug/deps/guessing_game-36b0db37a4334753.d: src/main.rs
-
-src/main.rs:
--- guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/Users/vk/software/rust/guessing_game/target/debug/guessing_game.dSYM: /Users/vk/software/rust/guessing_game/src/main.rs
