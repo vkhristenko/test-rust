175c6cc26794c1d5fd010c933d7162bbd858d77c
vkhristenko <vdkhristenko1991@gmail.com>
2018-04-22 18:00:54 +0200

    del

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -23,41 +23,5 @@ fn main() {
         let third: &i32 = &v[2];
         let third: i32 = v[2];
         let third: Option<&i32> = v.get(2);
-
-        //let does_not_exist = &v[100];
-        let does_not_exist = v.get(100);
-    }
-
-    {
-        let mut v =vec![1,2,3,4,5];
-        let first = &v[0];
-    }
-
-    {
-        let v = vec![100, 32, 57];
-        for i in &v {
-            println!("{}", i);
-        }
-    }
-    {
-        let mut v = vec![100, 32, 57];
-        for i in &mut v {
-            *i += 50;
-            println!("{} {}", i, *i);
-        }
-    }
-
-    {
-        enum SpreadsheetCell {
-            Int(i32),
-            Float(f64),
-            Text(String),
-        };
-
-        let row = vec![
-            SpreadsheetCell::Int(3),
-            SpreadsheetCell::Text(String::from("blue")),
-            SpreadsheetCell::Float(10.12),
-        ];
     }
 }
