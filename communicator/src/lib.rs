f33e10132463b9e6a1009c4e8d1e4ed310a7149a
vkhristenko <vdkhristenko1991@gmail.com>
2018-04-22 09:41:38 +0200

    starting with modules

---
+++ communicator/src/lib.rs
@@ -0,0 +1,17 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
+mod network {
+    fn connect() {
+    }
+}
+
+mod client {
+    fn connect() {
+    }
+}
