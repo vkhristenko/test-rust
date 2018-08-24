e11b2f35fa873e2450390efc337f05f120e22033
vkhristenko <vdkhristenko1991@gmail.com>
2018-08-24 17:18:11 +0200

    FILE* as void pointer

--- ffi/src/main.rs
+++ ffi/src/main.rs
@@ -6,12 +6,20 @@ struct Point {
     y: i32
 }
 
+#[repr(C)]
+struct FileContext {
+    file: *mut libc::c_void
+}
+
 #[link(name = "testrustc")]
 extern {
     fn add_int(a: i32, b: i32) -> i32;
 
     fn show_point(p: Point);
     fn show_point_byref(p: *mut Point);
+
+    fn open_context_toread() -> FileContext;
+    fn close_context(ctx: FileContext);
 }
 
 fn main() {
@@ -28,5 +36,10 @@ fn main() {
 
         let mut p2 = Point {x: 10, y:20};
         show_point_byref(&mut p2);
+    }
+
+    unsafe {
+        let ctx = open_context_toread();
+        close_context(ctx);
     }
 }
