c507039d451b9c3916637c7fb756b3f9accc9404
vkhristenko <vdkhristenko1991@gmail.com>
2018-08-24 17:08:07 +0200

    test c structs

--- ffi/src/main.rs
+++ ffi/src/main.rs
@@ -1,8 +1,17 @@
 extern crate libc;
 
+#[repr(C)]
+struct Point {
+    x: i32,
+    y: i32
+}
+
 #[link(name = "testrustc")]
 extern {
     fn add_int(a: i32, b: i32) -> i32;
+
+    fn show_point(p: Point);
+    fn show_point_byref(p: *mut Point);
 }
 
 fn main() {
@@ -11,5 +20,13 @@ fn main() {
     unsafe {
         let c = add_int(5, 6);
         println!("c = {}", c);
+    }
+
+    unsafe {
+        let p1 = Point {x: 10, y: 20};
+        show_point(p1);
+
+        let mut p2 = Point {x: 10, y:20};
+        show_point_byref(&mut p2);
     }
 }
