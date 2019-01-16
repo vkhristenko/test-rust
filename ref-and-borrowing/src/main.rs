9d369a21d61f2da7f0bec39b2bb44bd8b6a329a3
vkhristenko <vdkhristenko1991@gmail.com>
2019-01-16 10:20:54 +0100

    ref/borrowing

--- ref-and-borrowing/src/main.rs
+++ ref-and-borrowing/src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
     println!("Hello, world!");
+
+    let s1 = String::from("hello");
+    let len = calculate_length(&s1);
+
+    println!("the lenght of '{}' is {}.", s1, len);
+
+    test0();
+}
+
+fn test0() {
+    let mut s1 = String::from("hello");
+
+    change(&mut s1);
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+    // ^^^^^^^^^^^ `some_string` is a `&` reference, so the data it refers to cannot be borrowed as mutable
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
