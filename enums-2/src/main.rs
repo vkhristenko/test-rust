72b5bbcdb4ceb18414905c30cf28b6f050216a55
vkhristenko <vdkhristenko1991@gmail.com>
2019-01-18 10:17:56 +0100

    done with structs, started enums

---
+++ enums-2/src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    println!("Hello, world!");
+
+    test1();
+}
+
+enum IpAddrKind {
+    V4, V6
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+fn test1() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+}
--- structs-2/src/main.rs
+++ structs-2/src/main.rs
@@ -3,6 +3,7 @@ fn main() {
 
     test0();
     test1();
+    test2();
 }
 
 fn test1() {
@@ -27,6 +28,35 @@ fn test1() {
 
 struct Color(i32, i32, i32);
 struct Point(i32, i32, i32);
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32, 
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn make_square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
+}
+
+fn test2() {
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let sq = Rectangle::make_square(10);
+
+    println!("rect1 is {:#?}", rect1);
+    println!("area of rect is {}", rect1.area());
+    println!("area of rect is {}", (&rect1).area());
+}
 
 fn test3() {
     let black = Color(0, 0, 0);
