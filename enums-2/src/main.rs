ab62c2c183452a734d1e0480c9d4912f2ea103db
vkhristenko <vdkhristenko1991@gmail.com>
2019-01-20 18:41:23 +0100

    tested match and if let

--- enums-2/src/main.rs
+++ enums-2/src/main.rs
@@ -62,3 +62,46 @@ fn test2() {
 
     let absent_number: Option<i32> = None;
 }
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("state quarter from {:?}", state);
+            25
+        },
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn test4() {
+    let some_u8_value = Some(0u8);
+    match some_u8_value {
+        Some(3) => println!("three"),
+        _ => ()
+    }
+    if let Some(3) = some_u8_value {
+        println!("three");
+    }
+}
