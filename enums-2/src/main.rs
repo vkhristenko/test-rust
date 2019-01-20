f17b83a9cd21dc64ba80866ca4ea17b46436de22
vkhristenko <vdkhristenko1991@gmail.com>
2019-01-20 18:16:37 +0100

    testing enums

--- enums-2/src/main.rs
+++ enums-2/src/main.rs
@@ -8,9 +8,32 @@ enum IpAddrKind {
     V4, V6
 }
 
+enum IpAddr2 {
+    V4(String),
+    V6(String),
+}
+
+enum IpAddr3 {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
 struct IpAddr {
     kind: IpAddrKind,
     address: String,
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        // method body
+    }
 }
 
 fn test1() {
@@ -25,4 +48,17 @@ fn test1() {
         kind: IpAddrKind::V6,
         address: String::from("::1"),
     };
+    
+    let home2 = IpAddr2::V4(String::from("127.0.0.1"));
+    let loopback2 = IpAddr2::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+}
+
+fn test2() {
+    let some_number = Some(5);
+    let some_string = Some("a string");
+
+    let absent_number: Option<i32> = None;
 }
