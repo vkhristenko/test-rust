d764bb381ca3b82662c0bb36aa085011950c48e3
vkhristenko <vdkhristenko1991@gmail.com>
2019-01-28 08:39:26 +0100

    tested channels

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,10 +1,15 @@
 use std::thread;
 use std::time::Duration;
+use std::sync::mpsc;
 
 fn main() {
     println!("Hello, world!");
 
     test0();
+    test1();
+    test2();
+    test3();
+    test4();
 }
 
 fn test0() {
@@ -31,4 +36,74 @@ fn test1() {
     });
 
     handle.join().unwrap();
+}
+
+fn test2() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
+
+    let received = rx.recv().unwrap();
+    println!("got: {}", received);
+}
+
+fn test3() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in  rx {
+        println!("got: {}", received);
+    }
+}
+
+fn test4() {
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = mpsc::Sender::clone(&tx);
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("got: {}", received);
+    }
 }
