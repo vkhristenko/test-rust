5aee6f64fde0f7c544a1a47bce829263828dc587
vkhristenko <vdkhristenko1991@gmail.com>
2019-01-30 07:38:04 +0100

    testing trait objects (dyn <Trait>)

--- oop/src/main.rs
+++ oop/src/main.rs
@@ -16,7 +16,20 @@ impl AverageCollection {
     pub fn remove(&mut self) -> Option<i32> {
         let result = self.list.pop();
         match result {
-
+            Some(value) => {
+                self.update_average();
+                Some(value)
+            },
+            None => None,
         }
+    }
+
+    pub fn average(&self) -> f64 {
+        self.average
+    }
+
+    fn update_average(&mut self) {
+        let total: i32 = self.list.iter().sum();
+        self.average = total as f64 / self.list.len() as f64;
     }
 }
