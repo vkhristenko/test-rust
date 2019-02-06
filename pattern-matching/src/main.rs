ec9f70ad4b7d1a904c7ef233f8af21b2a5df9955
vkhristenko <vdkhristenko1991@gmail.com>
2019-02-06 09:31:58 +0100

    pattern matching: using match to destructure

--- pattern-matching/src/main.rs
+++ pattern-matching/src/main.rs
@@ -4,6 +4,14 @@ fn main() {
     test0();
     test1();
     test2();
+    test3();
+    test4();
+    test5();
+    test6();
+    test7();
+    test8();
+    test9();
+    test10();
 }
 
 fn test0() {
@@ -52,6 +60,120 @@ fn print_coordinates(&(x, y): &(i32, i32)) {
     println!("current location ({}, {})", x, y);
 }
 
+fn test3() {
+    let y: Option<_> = Some(5);
+//    let Some(x) = y;
+
+    if let Some(x) = y {
+        println!("x = {}", x);
+    }
+}
+
 fn foo(x: i32) {
 
+}
+
+fn test4() {
+    let x = 1;
+
+    match x {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        4 => println!("four"),
+        _ => println!("anything"),
+    }
+}
+
+fn test5() {
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("got 50"),
+        Some(y) => println!("matched, y = {:?}", y),
+        _ => println!("default case, x = {:?}", x),
+    }
+
+    println!("at the end, {:?}, y = {:?}", x , y);
+}
+
+fn test6() {
+    let x = 1;
+
+    match x {
+        1 | 2 => println!("one or two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+
+    match x {
+        1 ... 5 => println!("one through five"),
+        _ => println!("soemthing else"),
+    }
+}
+
+fn test7() {
+    let x = 'c';
+
+    match x {
+        'a' ... 'j' => println!("early ascii letters"),
+        'k' ... 'z' => println!("late ascii letters"),
+        _ => println!("something else"),
+    }
+}
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+fn test8() {
+    let p = Point { x: 0, y: 7 };
+
+    let Point { x: a, y: b } = p;
+    assert_eq!(0, a);
+    assert_eq!(7, b);
+}
+
+fn test9() {
+    let p = Point { x: 0, y: 7 };
+
+    let Point { x, y } = p;
+    
+    match p {
+        Point { x, y: 0 } => println!("on the x axis at {}", x),
+        Point { x: 0, y } => println!("on the y axis at {}", y),
+        Point { x, y } => println!("on neither axis: ({}, {})", x, y),
+    }
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+fn test10() {
+    let msg = Message::ChangeColor(0, 160, 255);
+
+    match msg {
+        Message::Quit => {
+            println!("the quit variant has no data to destructure");
+        },
+        Message::Move { x, y } => {
+            println!(
+                "move in the x direction {} and in the y direction {}",
+                x,
+                y);
+        },
+        Message::Write(text) => println!("text message: {}", text),
+        Message::ChangeColor(r, g, b) => {
+            println!("change the color to red {}, gree {}, blue {}",
+                     r,
+                     g,
+                     b);
+        }
+    }
 }
