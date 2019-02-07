8c37eb606e757b9e879a0a9eb1a789c51293a264
vkhristenko <vdkhristenko1991@gmail.com>
2019-02-07 10:18:06 +0100

    finished with pattern matching

--- pattern-matching/src/main.rs
+++ pattern-matching/src/main.rs
@@ -12,6 +12,10 @@ fn main() {
     test8();
     test9();
     test10();
+    test11();
+    test12();
+    test13();
+    test14();
 }
 
 fn test0() {
@@ -176,4 +180,62 @@ fn test10() {
                      b);
         }
     }
+}
+
+fn test11() {
+    let points = vec![
+        Point { x: 0, y: 0 },
+        Point { x: 1, y: 5 },
+        Point { x: 10, y: -3 },
+    ];
+
+    let sum_of_squares: i32 = points
+        .iter()
+        .map(|&Point {x, y}| x*x + y*y)
+        .sum();
+}
+
+fn test12() {
+    let num = Some(10);
+
+    match num {
+        Some(x) if x < 20 => println!("less than twenty {}", x),
+        Some(x) => println!("{}", x),
+        None => (),
+    }
+}
+
+fn test13() {
+    enum Message {
+        Hello { id: i32 },
+    };
+
+    let msg = Message::Hello{ id: 5 };
+    match msg {
+        Message::Hello { id: id_variable @ 3...7 } => {
+            println!("found an id in range: {}", id_variable)
+        },
+        Message::Hello { id: 10...12 } => {
+            println!("found an id in another range")
+        },
+        Message::Hello { id } => {
+            println!("found some other id: {}", id)
+        },
+    }
+}
+
+fn test14() {
+    let robot_name = &Some(String::from("Bors"));
+
+    match robot_name {
+        Some(name) => println!("found a name: {}", name),
+        None => (),
+    }
+
+    match robot_name {
+        &Some(ref name) => println!("found a name: {}", name),
+        None => ()r
+    }
+
+    println!("robot_name is {:?}", robot_name);
 }
