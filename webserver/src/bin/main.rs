ab74bce725e425aa8c90502939100938c1dd5365
vkhristenko <vdkhristenko1991@gmail.com>
2019-02-09 13:47:35 +0100

    thread pool impl + using channels to communicate tasks to the threads

--- webserver/src/bin/main.rs
+++ webserver/src/bin/main.rs
@@ -2,16 +2,23 @@ use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
 use std::fs;
+use std::thread;
+use std::time::Duration;
+use webserver::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
-        println!("connection established");
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
+
+    println!("main: shutting down");
 }
 
 fn handle_connection(mut stream: TcpStream) {
@@ -19,7 +26,7 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.read(&mut buffer).unwrap();
 
-    println!("request: {}", String::from_utf8_lossy(&buffer[..]));
+    //println!("request: {}", String::from_utf8_lossy(&buffer[..]));
 
     /*
     let get = b"GET / HTTP/1.1\r\n";
@@ -39,7 +46,12 @@ fn handle_connection(mut stream: TcpStream) {
     }*/
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
